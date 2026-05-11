a2511a7d9b9c419f9b58917683c7c4338dcf7c64
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2026-05-11 12:31:42 +0530

    fix harshyadav naming

--- devres/android/app/src/main/kotlin/com/example/devres/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.devres
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ devres/android/app/src/main/kotlin/com/harshyadav/devres/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.harshyadav.devres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
